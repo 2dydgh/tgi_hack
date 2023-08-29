3a30cd0cdb2e726a46f55cbb8e44aabd884089a3
Zoa Young Kwon <kzoyoung@naver.com>
2023-08-29 16:07:21 +0900

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+// game config
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#b6d53c',
+    parent: 'phaser-example',
+    physics: { default: "arcade" },
+    pixelArt: true,
+    scene: [ GameScene ],
+};
+
+// instantiate the game
+const game = new Phaser.Game(config);
