8be297af311a25c28b652d06ec3d34659824c806
Zoa Young Kwon <Kzoyoung@naver.com>
2023-08-30 09:42:43 +0900

    feat:수정

--- index.ts
+++ index.ts
@@ -51,6 +51,54 @@ export class GameScene extends Phaser.Scene {
     this.load.html('coupon', hidden_coupon);
   }
 
+  createSpeechBubble (x, y, width, height, quote)
+    {
+        const bubbleWidth = width;
+        const bubbleHeight = height;
+        const bubblePadding = 10;
+        const arrowHeight = bubbleHeight / 4;
+
+        const bubble = this.add.graphics({ x: x, y: y });
+
+        //  Bubble shadow
+        bubble.fillStyle(0x222222, 0.5);
+        bubble.fillRoundedRect(6, 6, bubbleWidth, bubbleHeight, 16);
+
+        //  Bubble color
+        bubble.fillStyle(0xffffff, 1);
+
+        //  Bubble outline line style
+        bubble.lineStyle(4, 0x565656, 1);
+
+        //  Bubble shape and outline
+        bubble.strokeRoundedRect(0, 0, bubbleWidth, bubbleHeight, 16);
+        bubble.fillRoundedRect(0, 0, bubbleWidth, bubbleHeight, 16);
+
+        //  Calculate arrow coordinates
+        const point1X = Math.floor(bubbleWidth / 7);
+        const point1Y = bubbleHeight;
+        const point2X = Math.floor((bubbleWidth / 7) * 2);
+        const point2Y = bubbleHeight;
+        const point3X = Math.floor(bubbleWidth / 7);
+        const point3Y = Math.floor(bubbleHeight + arrowHeight);
+
+        //  Bubble arrow shadow
+        bubble.lineStyle(4, 0x222222, 0.5);
+        bubble.lineBetween(point2X - 1, point2Y + 6, point3X + 2, point3Y);
+
+        //  Bubble arrow fill
+        bubble.fillTriangle(point1X, point1Y, point2X, point2Y, point3X, point3Y);
+        bubble.lineStyle(2, 0x565656, 1);
+        bubble.lineBetween(point2X, point2Y, point3X, point3Y);
+        bubble.lineBetween(point1X, point1Y, point3X, point3Y);
+
+        const content = this.add.text(0, 0, quote, { fontFamily: 'Arial', fontSize: 20, color: '#000000', align: 'center', wordWrap: { width: bubbleWidth - (bubblePadding * 2) } });
+
+        const b = content.getBounds();
+
+        content.setPosition(bubble.x + (bubbleWidth / 2) - (b.width / 2), bubble.y + (bubbleHeight / 2) - (b.height / 2));
+    }
+
   create() {
     const api_key = '3bad6ce02bbd4c3582a41d972f37338f';
     const base_url = 'https://api.rawg.io/api/';
@@ -125,6 +173,20 @@ export class GameScene extends Phaser.Scene {
     ];
 
     const frameRate = 10;
+
+    this.anims.create({
+      key: 'idle_down2',
+      frames: this.anims.generateFrameNames('character2', {
+        prefix: 'Lucy_idle_anim_',
+        start: 19,
+        end: 24,
+        zeroPad: 0,
+        suffix: '.png',
+      }),
+      frameRate,
+      repeat: -1,
+    });
+
     this.anims.create({
       key: 'sit_down',
       frames: this.anims.generateFrameNames('character', {
@@ -242,7 +304,7 @@ export class GameScene extends Phaser.Scene {
       'Adam_idle_anim_21.png'
     );
 
-    this.physics.add.sprite(1100, 3100, 'character2');
+    this.physics.add.sprite(1100, 3100, 'character2').anims.play("idle_down2", true);
     this.player.anims.play('idle_down', true);
 
     this.cameras.main.zoom = 1.0;
@@ -535,14 +597,14 @@ export class GameScene extends Phaser.Scene {
 
       const button1 = document.createElement('button');
       button1.innerText = 'Singleplayer가좋아';
-      button1.style.width = '100px';
+      button1.style.width = '150px';
       button1.style.height = '50px';
       button1.style.fontSize = '18px';
       element1.node.appendChild(button1);
 
       const button2 = document.createElement('button');
       button2.innerText = '게임은multiplayer';
-      button2.style.width = '100px';
+      button2.style.width = '150px';
       button2.style.height = '50px';
       button2.style.fontSize = '18px';
       element1.node.appendChild(button2);
@@ -598,6 +660,7 @@ export class GameScene extends Phaser.Scene {
             repeatDelay: 500,
             ease: 'linear',
           });
+          this.createSpeechBubble(1050, 2950, 250, 100, '“반가워”');
         }, 2000);
       });
       this.iscreated = true;
